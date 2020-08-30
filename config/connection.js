3e36594d06ba5ba0cd1781405d8362d24ab75ca8
John Manouse <johnmanouse@Macintosh-2.local>
2020-08-30 12:55:13 -0400

    sql connection

---
+++ config/connection.js
@@ -0,0 +1,32 @@
+//sql connection
+const mysql = require("mysql");
+require("dotenv").config();
+
+let connection;
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+
+} else {
+    connection = mysql.createConnection({
+        host: process.env.DB_HOST,
+
+        port: process.env.DB_PORT,
+
+        user: process.env.DB_USERNAME,
+
+        password: process.env.DB_PASSWORD,
+        
+        database: process.env.DB_NAME
+    });
+}
+
+connection.connect(function(err){
+    if (err) {
+        console.error("connection error: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
+//export connection
+module.exports = connection;
