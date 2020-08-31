0fb5e19edbe6275b0c8706872f66ffde8db06ef4
John Manouse <johnmanouse@Macintosh-2.local>
2020-08-31 09:58:53 -0400

    update orm

--- config/orm.js
+++ config/orm.js
@@ -28,5 +28,58 @@ for (const key in ob) {
         arr.push(key + "=" + value);
         }
     }
+//convert arrays to single string
     return arr.toString();
 }
+//sql statement functions object
+const orm = {
+    all: function(tableInput, cb) {
+        const queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err;
+            }
+        cb(result);
+    });
+    },
+    
+create: function(table, cols, vals, cb){
+    val[1] = false;
+    let queryString = "INSERT INTO " + table;
+    queryString += " (";
+    queryString += cols.toString();
+    queryString += ") ";
+    queryString += "VALUES (";
+    queryString += printQuestionMarks(vals.length);
+    queryString += ") ";
+
+    connection.query(queryString, vals, function(err, result) {
+        if (err) {
+          throw err;
+        }
+  
+        cb(result);
+    });
+},
+
+
+update: function(table, condition, cb) {
+    console.log(condition);
+
+    let queryString = "UPDATE" + table;
+
+    queryString += " SET devoured = true";
+    queryString += " WHERE ";
+    queryString += condition;
+
+    console.log(queryString);
+    connection.query(queryString, function(err, result) {
+        if (err) {
+            throw err;
+        }
+        cb(result);
+    });
+  }
+};
+//export orm to model
+module.exports = orm;
